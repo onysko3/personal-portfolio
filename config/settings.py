54f510aa27da045f0d2a5f478c84c6df7d64744a
Oleh Onysko <oleh.onysko3@gmail.com>
2021-07-04 09:17:25 +0200

    added allowed_hosts

--- config/settings.py
+++ config/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = ['nysk.xyz', '.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['.nysk.xyz', 'nysk.xyz', '.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
